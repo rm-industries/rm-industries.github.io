a9a1d0d254fa5b67fe6130e0c25f8480930ee4ae
Rahul Mohandas <rahul0705@gmail.com>
2026-09-24 08:36:16 -0400

    test: scope company logo selector

--- tests/brand.spec.ts
+++ tests/brand.spec.ts
@@ -5,7 +5,7 @@ import { resolvePreviewPath } from './preview';
 test('renders company marks without distortion', async ({ page }) => {
   await page.goto(resolvePreviewPath('/'));
 
-  const marks = page.locator('img[src$="/logo.svg"]');
+  const marks = page.locator('img[src="/logo.svg"]');
   await expect(marks).toHaveCount(2);
 
   const dimensions = await marks.evaluateAll((images) =>
